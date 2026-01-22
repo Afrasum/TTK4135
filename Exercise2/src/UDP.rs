35e252f8c5269bf99538b1e6fb85d25dccc79935
Afras Mansoor <afras@hotmail.no>
2026-01-22 18:17:11 +0100

    feat: UDP done

--- Exercise2/src/UDP.rs
+++ Exercise2/src/UDP.rs
@@ -8,6 +8,13 @@ fn main() {
 
     let local_ip: IpAddr = "192.168.1.50".parse().unwrap();
 
+    let server_addr: SocketAddr = "127.0.0.1:20000".parse().unwrap();
+
+    let sock: UdpSocket = UdpSocket::bind("0.0.0.0:0").unwrap();
+
+    sock.connect(server_addr).unwrap();
+    let mut i = 0;
+
     loop {
         // numBytesReceived = recvSock.receiveFrom(buffer, ref fromWho)
         let (num_bytes_received, from_who) = recieve_socket.recv_from(&mut buffer).unwrap();
@@ -16,5 +23,10 @@ fn main() {
         let msg = std::str::from_utf8(&buffer[..num_bytes_received]).unwrap();
         println!("Received: {}", msg);
         // }
+
+        let msg_to_send = format!("Hello {}", i);
+
+        sock.send(msg_to_send.as_bytes()).unwrap();
+        i += 1;
     }
 }
